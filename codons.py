0bcc6fecf5fa6029cfeb3462b76480634480fe7f
moranshmaya <moranshmaya@gmail.com>
2025-11-27 13:45:13 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,12 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    codons_dic = {}
+    file = open(file_path)
+    rows = file.readlines()
+    file.close()
+    for row in rows:
+        row = row.strip()
+        cells = row.split("\t")
+        codons_dic.update({cells[0]:cells[2]})
+    return codons_dic
 
 
